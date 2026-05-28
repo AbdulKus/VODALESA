e5ea170991d5b3ab9452d81a8ff57ead4547e4e0
itwasnt2120-ai <itwasnt2120@gmail.com>
2026-05-28 20:41:18 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.VODALESA_CONFIG = {
   ticketsUrl: "https://ticketscloud.com/v1/widgets/common?token=eyJhbGciOiJIUzI1NiIsImlzcyI6InRpY2tldHNjbG91ZC5ydSIsInR5cCI6IkpXVCJ9.eyJwIjoiNjlkOGY5ZTEzZTIzMGIwYmQ1ZDQyNTJmIn0.TWwQDeab7PKB0wrZOLu1Jmp1VEjZn1AI-mdEDvTZ9YM&lang=ru&event=6a182cadc1103eb5849b605a&s=1",
-  buttonText: "Купить билет"
+  buttonText: "Разбить еблет"
 };
