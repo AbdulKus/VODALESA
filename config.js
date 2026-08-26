0197eb26f5b1eeed3f56a3231713a1103698fcf7
itwasnt2120-ai <itwasnt2120@gmail.com>
2026-08-26 13:09:42 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.VODALESA_CONFIG = {
-  ticketsUrl: "https://qtickets.ru/organizer/46999?base_color=658bd0",
+  ticketsUrl: "https://moscow.qtickets.events/254607-vodalesa",
   buttonText: "Купить билет",
   artists: "Kedr Livanskiy, Sir Soylok, Карнегия, Нелюдимка, телеэкран, apollo eastman+, Деревянный старик Митрей, Dirty Glassez, lsd prizrak, nl.DEFRAQ, Ускоритель частиц, Ptichka Soe, сине сине небо, калина красная, Хайлайтер, trat, покой, Звонь, Реклама снов"
 };
